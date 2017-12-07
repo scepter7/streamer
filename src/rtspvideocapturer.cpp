da6f6e1048e19211a27746f995a357274d4a5a54
bhlowe <bhlowe@gmail.com>
2017-12-07 13:07:11 -0800

    Switch on NAL_Unit type to discard unused.

--- src/rtspvideocapturer.cpp
+++ src/rtspvideocapturer.cpp
@@ -97,6 +97,10 @@ bool RTSPVideoCapturer::onNewSession(const char* id,const char* media, const cha
 	return success;
 }
 
+
+
+
+
 bool RTSPVideoCapturer::onData(const char* id, unsigned char* buffer, ssize_t size, struct timeval presentationTime)
 {
 	int64_t ts = presentationTime.tv_sec;
@@ -109,58 +113,72 @@ bool RTSPVideoCapturer::onData(const char* id, unsigned char* buffer, ssize_t si
 		int nal_end   = 0;
 		find_nal_unit(buffer, size, &nal_start, &nal_end);
 		read_nal_unit(m_h264, &buffer[nal_start], nal_end - nal_start);
-		if (m_h264->nal->nal_unit_type == NAL_UNIT_TYPE_SPS) {
-			RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData SPS";
-			m_cfg.clear();
-			m_cfg.insert(m_cfg.end(), buffer, buffer+size);
+		switch(m_h264->nal->nal_unit_type)
+		{
+			case NAL_UNIT_TYPE_SPS:
+			{
+				RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData SPS";
+				m_cfg.clear();
+				m_cfg.insert(m_cfg.end(), buffer, buffer+size);
 
-			unsigned int width = ((m_h264->sps->pic_width_in_mbs_minus1 +1)*16) - m_h264->sps->frame_crop_left_offset*2 - m_h264->sps->frame_crop_right_offset*2;
-			unsigned int height= ((2 - m_h264->sps->frame_mbs_only_flag)* (m_h264->sps->pic_height_in_map_units_minus1 +1) * 16) - (m_h264->sps->frame_crop_top_offset * 2) - (m_h264->sps->frame_crop_bottom_offset * 2);
-			unsigned int fps=25;
-			RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData SPS set timing_info_present_flag:" << m_h264->sps->vui.timing_info_present_flag << " " << m_h264->sps->vui.time_scale << " " << m_h264->sps->vui.num_units_in_tick;
-			if (m_decoder.get()) {
-				if ( (GetCaptureFormat()->width != width) || (GetCaptureFormat()->height != height) )  {
-					RTC_LOG(INFO) << "format changed => set format from " << GetCaptureFormat()->width << "x" << GetCaptureFormat()->height	 << " to " << width << "x" << height;
-					m_decoder.reset(NULL);
+				unsigned int width = ((m_h264->sps->pic_width_in_mbs_minus1 +1)*16) - m_h264->sps->frame_crop_left_offset*2 - m_h264->sps->frame_crop_right_offset*2;
+				unsigned int height= ((2 - m_h264->sps->frame_mbs_only_flag)* (m_h264->sps->pic_height_in_map_units_minus1 +1) * 16) - (m_h264->sps->frame_crop_top_offset * 2) - (m_h264->sps->frame_crop_bottom_offset * 2);
+				unsigned int fps=25;
+				RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData SPS set timing_info_present_flag:" << m_h264->sps->vui.timing_info_present_flag << " " << m_h264->sps->vui.time_scale << " " << m_h264->sps->vui.num_units_in_tick;
+				if (m_decoder.get()) {
+					if ( (GetCaptureFormat()->width != width) || (GetCaptureFormat()->height != height) )  {
+						RTC_LOG(INFO) << "format changed => set format from " << GetCaptureFormat()->width << "x" << GetCaptureFormat()->height	 << " to " << width << "x" << height;
+						m_decoder.reset(NULL);
+					}
+				}
+
+				if (!m_decoder.get()) {
+					RTC_LOG(INFO) << "RTSPVideoCapturer:onData SPS set format " << width << "x" << height << " fps:" << fps;
+					cricket::VideoFormat videoFormat(width, height, cricket::VideoFormat::FpsToInterval(fps), cricket::FOURCC_I420);
+					SetCaptureFormat(&videoFormat);
+
+					m_decoder=m_factory.CreateVideoDecoder(webrtc::SdpVideoFormat(cricket::kH264CodecName));
+					webrtc::VideoCodec codec_settings;
+					codec_settings.codecType = webrtc::VideoCodecType::kVideoCodecH264;
+					m_decoder->InitDecode(&codec_settings,2);
+					m_decoder->RegisterDecodeCompleteCallback(this);
 				}
 			}
+				break;
+			case NAL_UNIT_TYPE_PPS:
+				RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData PPS";
+				m_cfg.insert(m_cfg.end(), buffer, buffer+size);
+				break;
+			case NAL_UNIT_TYPE_AUD:
+			case NAL_UNIT_TYPE_SEI:
+				break;
+			case NAL_UNIT_TYPE_CODED_SLICE_IDR:
+				if (m_decoder.get()) {
+					uint8_t buf[m_cfg.size() + size];
+					memcpy(buf, m_cfg.data(), m_cfg.size());
+					memcpy(buf+m_cfg.size(), buffer, size);
+					webrtc::EncodedImage input_image(buf, sizeof(buf), sizeof(buf) + webrtc::EncodedImage::GetBufferPaddingBytes(webrtc::VideoCodecType::kVideoCodecH264));
+					input_image._timeStamp = ts*1000;
+					RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData IDR ts="<<(ts*1000);
+					res = m_decoder->Decode(input_image, false, NULL);
+				} else 	{
+					RTC_LOG(LS_ERROR) << "RTSPVideoCapturer:onData no decoder";
+					res = -1;	
+				}
+				break;
+			default:
+				if (m_decoder.get()) {
+					webrtc::EncodedImage input_image(buffer, size, size + webrtc::EncodedImage::GetBufferPaddingBytes(webrtc::VideoCodecType::kVideoCodecH264));
+					input_image._timeStamp = ts*1000;
+					RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData SLICE NALU:" << m_h264->nal->nal_unit_type << " ts=" << input_image._timeStamp;
+					res = m_decoder->Decode(input_image, false, NULL);
+				}else 	{
+					RTC_LOG(LS_ERROR) << "RTSPVideoCapturer:onData no decoder";
+					res = -1;	
+				}
+				
+		}
 
-			if (!m_decoder.get()) {
-				RTC_LOG(INFO) << "RTSPVideoCapturer:onData SPS set format " << width << "x" << height << " fps:" << fps;
-				cricket::VideoFormat videoFormat(width, height, cricket::VideoFormat::FpsToInterval(fps), cricket::FOURCC_I420);
-				SetCaptureFormat(&videoFormat);
-
-				m_decoder=m_factory.CreateVideoDecoder(webrtc::SdpVideoFormat(cricket::kH264CodecName));
-				webrtc::VideoCodec codec_settings;
-				codec_settings.codecType = webrtc::VideoCodecType::kVideoCodecH264;
-				m_decoder->InitDecode(&codec_settings,2);
-				m_decoder->RegisterDecodeCompleteCallback(this);
-			}
-		}
-		else if (m_h264->nal->nal_unit_type == NAL_UNIT_TYPE_PPS) {
-			RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData PPS";
-			m_cfg.insert(m_cfg.end(), buffer, buffer+size);
-		}
-		else if (m_decoder.get()) {
-			if (m_h264->nal->nal_unit_type == NAL_UNIT_TYPE_CODED_SLICE_IDR) {
-				RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData IDR";
-				uint8_t buf[m_cfg.size() + size];
-				memcpy(buf, m_cfg.data(), m_cfg.size());
-				memcpy(buf+m_cfg.size(), buffer, size);
-				webrtc::EncodedImage input_image(buf, sizeof(buf), sizeof(buf) + webrtc::EncodedImage::GetBufferPaddingBytes(webrtc::VideoCodecType::kVideoCodecH264));
-				input_image._timeStamp = ts*1000;
-				res = m_decoder->Decode(input_image, false, NULL);
-			}
-			else {
-				RTC_LOG(LS_VERBOSE) << "RTSPVideoCapturer:onData SLICE NALU:" << m_h264->nal->nal_unit_type;
-				webrtc::EncodedImage input_image(buffer, size, size + webrtc::EncodedImage::GetBufferPaddingBytes(webrtc::VideoCodecType::kVideoCodecH264));
-				input_image._timeStamp = ts*1000;
-				res = m_decoder->Decode(input_image, false, NULL);
-			}
-		} else {
-			RTC_LOG(LS_ERROR) << "RTSPVideoCapturer:onData no decoder";
-			res = -1;
-		}
 	} else if (m_codec == "JPEG") {
 		int32_t width = 0;
 		int32_t height = 0;
